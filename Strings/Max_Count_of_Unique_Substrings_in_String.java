2490f9ddbb44180c4d56bfa3f566afac384ef01e
Mayappa <mayappapujari561999@gmail.com>
2024-10-21 17:59:28 +0530

    added new code

--- Strings/Max_Count_of_Unique_Substrings_in_String.java
+++ Strings/Max_Count_of_Unique_Substrings_in_String.java
@@ -1,14 +1,22 @@
-
 import java.util.*;
 
 public class Max_Count_of_Unique_Substrings_in_String {
 
     public static int maxUniqueSplit(String s) {
-        return backtrack(s, 0, new HashSet<>());
+        List<String> result = new ArrayList<>(); 
+        int maxLength = backtrack(s, 0, new HashSet<>(), new ArrayList<>(), result);
+
+        System.out.println("Unique substrings: " + result);
+        return maxLength;
     }
 
-    private static int backtrack(String s, int start, HashSet<String> set) {
+    private static int backtrack(String s, int start, HashSet<String> set, List<String> currentList,
+            List<String> result) {
         if (start == s.length()) {
+            if (currentList.size() > result.size()) {
+                result.clear();
+                result.addAll(currentList);
+            }
             return set.size();
         }
 
@@ -19,18 +27,22 @@ public class Max_Count_of_Unique_Substrings_in_String {
 
             if (!set.contains(substring)) {
                 set.add(substring);
-                count = Math.max(count, backtrack(s, i, set));
+                currentList.add(substring);
+
+                count = Math.max(count, backtrack(s, i, set, currentList, result));
+
                 set.remove(substring);
+                currentList.remove(currentList.size() - 1);
             }
-            System.out.println(set);
-
         }
-
         return count;
     }
 
     public static void main(String[] args) {
-
-        System.out.println(maxUniqueSplit("ababccc"));
+        int maxLength = maxUniqueSplit("ababccc");
+        System.out.println("Maximum number of unique substrings: " + maxLength);
+        System.out.println();
+        int Length = maxUniqueSplit("aba");
+        System.out.println("Maximum number of unique substrings: " + Length);
     }
 }
