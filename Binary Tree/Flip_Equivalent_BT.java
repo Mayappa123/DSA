d8b27b7504339a351051bc691dc502b02a595cdb
Mayappa <mayappapujari561999@gmail.com>
2024-10-24 19:00:16 +0530

    added new file

---
+++ Binary Tree/Flip_Equivalent_BT.java
@@ -0,0 +1,47 @@
+public class Flip_Equivalent_BT {
+    // Definition of Node class for the custom binary tree
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean flipEquiv(Node root1, Node root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+
+        if (root1 == null || root2 == null || root1.data != root2.data) {
+            return false;
+        }
+        boolean noFlip = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+        boolean flip = flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+
+        return noFlip || flip;
+    }
+
+    public static void main(String[] args) {
+        Node root1 = new Node(1);
+        root1.left = new Node(2);
+        root1.right = new Node(3);
+        root1.left.left = new Node(4);
+        root1.left.right = new Node(5);
+        root1.right.left = new Node(6);
+        root1.right.right = new Node(7);
+
+        Node root2 = new Node(1);
+        root2.left = new Node(3);
+        root2.right = new Node(2);
+        root2.right.left = new Node(5);
+        root2.right.right = new Node(4);
+        root2.left.left = new Node(7);
+        root2.left.right = new Node(6);
+
+        System.out.println(flipEquiv(root1, root2)); // true
+    }
+}
