8ede1ad20e1086db5be25954f4552b53952402ce
Mayappa <mayappapujari561999@gmail.com>
2024-11-01 14:00:39 +0530

    added problem statement

--- Strings/Delete_Characters_to_Make_Fancy_String.java
+++ Strings/Delete_Characters_to_Make_Fancy_String.java
@@ -1,3 +1,10 @@
+
+// 1957. Delete Characters to Make Fancy String
+
+// A fancy string is a string where no three consecutive characters are equal.
+// Given a string s, delete the minimum possible number of characters from s to make it fancy.
+// Return the final string after the deletion. It can be shown that the answer will always be unique.
+
 public class Delete_Characters_to_Make_Fancy_String {
     public static String makeFancyString(String s) {
         StringBuilder result = new StringBuilder();
