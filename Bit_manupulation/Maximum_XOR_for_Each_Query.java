856ee62fb1109d6f85d202481a2ba0f34dbcd6cd
Mayappa <mayappapujari561999@gmail.com>
2024-11-08 21:43:31 +0530

    added new code

--- Bit_manupulation/Maximum_XOR_for_Each_Query.java
+++ Bit_manupulation/Maximum_XOR_for_Each_Query.java
@@ -1,3 +1,9 @@
+// You are given a sorted array nums of n non-negative integers and an integer maximumBit. You want to perform the following query n times:
+
+// Find a non-negative integer k < 2maximumBit such that nums[0] XOR nums[1] XOR ... XOR nums[nums.length-1] XOR k is maximized. k is the answer to the ith query.
+// Remove the last element from the current array nums.
+// Return an array answer, where answer[i] is the answer to the ith query.
+
 public class Maximum_XOR_for_Each_Query {
 
     public static int[] getMaximumXor(int[] nums, int maximumBit) {
