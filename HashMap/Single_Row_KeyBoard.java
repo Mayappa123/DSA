264998aae7b41d8e91de4fb0a8f2ac517bcadf76
Mayappa <mayappapujari561999@gmail.com>
2024-11-14 12:53:17 +0530

    added new files

--- HashMap/Single_Row_KeyBoard.java
+++ HashMap/Single_Row_KeyBoard.java
@@ -32,7 +32,21 @@ public class Single_Row_KeyBoard {
         String keyboard = "abcdefghijklmnopqrstuvwxy", text = "cba";
         int time = calculateTime(keyboard, text);
 
-        System.out.println(time); //3
+        System.out.println(time); // 4
 
     }
 }
+
+// Input:keyboard="abcdefghijklmnopqrstuvwxy",text="cba"Output:4 Explanation:
+
+// Initially your finger is at index 0.
+// First you have to type'c'.
+//
+// The time taken to type'c' will be abs(2 - 0) = 2 because character 'c' is at
+// index 2.
+
+// The second character is 'b' and your finger is now at index 2. The time taken
+// to type 'b' will be abs(1 - 2) = 1 because character 'b' is at index 1.
+// The third character is 'a' and your finger is now at index 1. The time taken
+// to type 'a' will be abs(0 - 1) = 1 because character 'a' is at index 0.
+// The total time will therefore be 2 + 1 + 1 = 4.
