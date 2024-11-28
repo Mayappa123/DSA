a9243e925c342e1efe361a4bf58af9291c417471
Mayappa <mayappapujari561999@gmail.com>
2024-11-28 20:06:57 +0530

    added new code

--- Matrix/Minimum_Obstacle_Remove_to_Reach_Corner.java
+++ Matrix/Minimum_Obstacle_Remove_to_Reach_Corner.java
@@ -1,5 +1,14 @@
-import java.util.*;
 
+// You are given a 0-indexed 2D integer array grid of size m x n. 
+//Each cell has one of two values:
+
+// 0 represents an empty cell,
+// 1 represents an obstacle that may be removed.
+// You can move up, down, left, or right from and to an empty cell.
+
+// Return the minimum number of obstacles to remove so you can move from the upper left corner (0, 0) to the lower right corner (m - 1, n - 1).
+
+import java.util.*;
 class Minimum_Obstacle_Remove_to_Reach_Corner {
     public static int minimumObstacles(int[][] grid) {
         int m = grid.length, n = grid[0].length;
