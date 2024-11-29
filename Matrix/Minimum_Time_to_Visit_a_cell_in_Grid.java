ffeea94b5d52a93d089dddbb1aa0f0635e76451e
Mayappa <mayappapujari561999@gmail.com>
2024-11-29 20:12:58 +0530

    added new file

--- Matrix/Minimum_Time_to_Visit_a_cell_in_Grid.java
+++ Matrix/Minimum_Time_to_Visit_a_cell_in_Grid.java
@@ -1,5 +1,16 @@
 //2577. Minimum Time to Visit a Cell In a Grid
 
+// You are given a m x n matrix grid consisting of non-negative integers 
+//where grid[row][col]represents the minimum time required to be able to visit the cell(row,col),
+//which means you can visit the cell(row,col)only when the time you visit it is greater than or equal to grid[row][col].
+
+// You are standing in the top-left cell of the matrix in the 0 th second,and 
+//you must move to any adjacent cell in the four directions:up,down,left,and right
+//.Each move you make takes 1 second.
+
+// Return the minimum time required in which you can visit the bottom-right cell of the matrix.
+//If you cannot visit the bottom-right cell,then return-1.
+
 import java.util.*;
 
 public class Minimum_Time_to_Visit_a_cell_in_Grid {
