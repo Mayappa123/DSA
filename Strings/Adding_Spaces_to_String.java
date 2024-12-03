84eef14d68a84e6c1bb7bc6d95fb95a887870b8d
Mayappa <mayappapujari561999@gmail.com>
2024-12-03 23:09:41 +0530

    added new code

--- Strings/Adding_Spaces_to_String.java
+++ Strings/Adding_Spaces_to_String.java
@@ -1,5 +1,12 @@
 //2109. Adding Spaces to a String
 
+// You are given a 0-indexed string s and a 0-indexed integer array spaces that describes the indices in the original string where spaces will be added. 
+//Each space should be inserted before the character at the given index.
+
+// For example, given s = "EnjoyYourCoffee" and spaces = [5, 9], we place spaces before 'Y' and 'C', which are at indices 5 and 9 respectively. 
+//Thus, we obtain "Enjoy Your Coffee".
+// Return the modified string after the spaces have been added.
+
 public class Adding_Spaces_to_String {
 
     public static String addSpaces(String s, int[] spaces) {
@@ -16,11 +23,12 @@ public class Adding_Spaces_to_String {
 
         return sb.toString();
     }
+
     public static void main(String[] args) {
 
         String s = "LeetcodeHelpsMeLearn";
-        int[] spaces = {8,13,15};
-        System.out.println(addSpaces(s, spaces));  // Leetcode Helps Me Learn
+        int[] spaces = { 8, 13, 15 };
+        System.out.println(addSpaces(s, spaces)); // Leetcode Helps Me Learn
 
     }
 }
