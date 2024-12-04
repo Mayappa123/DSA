d5d3776e28fdeb124dbda4f9a914b44c1852f318
Mayappa <mayappapujari561999@gmail.com>
2024-12-04 22:44:48 +0530

    added new chapter

---
+++ Strings/Can_Make_Subsequence.java
@@ -0,0 +1,19 @@
+public class Can_Make_Subsequence {
+    
+    public static boolean canMakeSubsequence(String source, String target) {
+        int targetIdx = 0, targetLen = target.length();
+
+        for (char currChar : source.toCharArray()) {
+            if (targetIdx < targetLen && (target.charAt(targetIdx) - currChar + 26) % 26 <= 1) {
+                targetIdx++;
+            }
+        }
+        return targetIdx == targetLen;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abc", str2 = "ad";
+
+        System.out.println(canMakeSubsequence(str1, str2));
+    }
+}
