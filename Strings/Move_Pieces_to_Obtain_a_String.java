8cad9e2480570440268286f140fe703b65e97406
Mayappa <mayappapujari561999@gmail.com>
2024-12-05 20:04:55 +0530

    added new files

--- Strings/Move_Pieces_to_Obtain_a_String.java
+++ Strings/Move_Pieces_to_Obtain_a_String.java
@@ -1,5 +1,13 @@
 //2337. Move Pieces to Obtain a String
 
+// You are given two strings start and target, both of length n. 
+//Each string consists only of the characters 'L', 'R', and '_' where:
+
+// The characters 'L' and 'R' represent pieces, where a piece 'L' can move to the left only if there is a blank space directly to its left.
+//A piece 'R' can move to the right only if there is a blank space directly to its right.
+// The character '_' represents a blank space that can be occupied by any of the 'L' or 'R' pieces.
+// Return true if it is possible to obtain the string target by moving the pieces of the string start any number of times. Otherwise, return false.
+
 public class Move_Pieces_to_Obtain_a_String {
     public static boolean canChange(String start, String target) {
         if (start.equals(target)) {
