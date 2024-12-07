38edd8abc443b9aa8a258e3437a1dd2740d5bef0
Mayappa <mayappapujari561999@gmail.com>
2024-12-07 16:42:09 +0530

    added new code

--- arrays/Minimum_Limit_of_Balls_in_Bag.java
+++ arrays/Minimum_Limit_of_Balls_in_Bag.java
@@ -42,10 +42,21 @@ public class Minimum_Limit_of_Balls_in_Bag {
 
         return operations <= maxOperations;
     }
+
     public static void main(String[] args) {
-        int[] nums = {9};
+        int[] nums = { 9 };
         int maxOperations = 2;
 
-        System.out.println(minimumSize(nums, maxOperations));
+        System.out.println(minimumSize(nums, maxOperations)); // 3
+
+        // Input: nums = [9], maxOperations = 2
+        // Output: 3
+        // Explanation:
+        // - Divide the bag with 9 balls into two bags of sizes 6 and 3. [9] -> [6,3].
+        // - Divide the bag with 6 balls into two bags of sizes 3 and 3. [6,3] ->
+        // [3,3,3].
+        // The bag with the most number of balls has 3 balls, so your penalty is 3 and
+        // you should return 3.
+
     }
 }
