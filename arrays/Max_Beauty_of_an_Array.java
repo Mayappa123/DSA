917901b25bf7444cc511cf5956923aecfca4e605
Mayappa <mayappapujari561999@gmail.com>
2024-12-11 13:22:41 +0530

    added new code

--- arrays/Max_Beauty_of_an_Array.java
+++ arrays/Max_Beauty_of_an_Array.java
@@ -1,5 +1,14 @@
-import java.util.Arrays;
 
+// You are given a 0-indexed array nums and a non-negative integer k.
+// In one operation,you can do the following:
+// Choose an index i that hasn'tbeen chosen before from the range[0,nums.length-1].
+//Replace nums[i]with any integer from the range[nums[i]-k,nums[i]+k].
+//The beauty of the array is the length of the longest subsequence consisting of equal elements.
+// Return the maximum possible beauty of the array nums after applying the operation any number of times.
+// Note that you can apply the operation to each index only once.
+// A subsequence of an array is a new array generated from the original array by deleting some elements(possibly none)without changing the order of the remaining elements.
+
+import java.util.Arrays;
 public class Max_Beauty_of_an_Array {
     public static int maximumBeauty(int[] nums, int k) {
         Arrays.sort(nums);
