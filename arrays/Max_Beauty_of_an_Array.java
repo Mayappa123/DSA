0d2b647f1227a5421061709185d2a80e5a79e94d
Mayappa <mayappapujari561999@gmail.com>
2024-12-11 13:20:50 +0530

    added new code

---
+++ arrays/Max_Beauty_of_an_Array.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Max_Beauty_of_an_Array {
+    public static int maximumBeauty(int[] nums, int k) {
+        Arrays.sort(nums);
+        int i = 0, j, n = nums.length;
+
+        for (j = 0; j < n; ++j) {
+            if (nums[j] - nums[i] > k * 2) {
+                i++;
+            }
+        }
+        return j - i;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 4, 6, 1, 2 };
+        int k = 2;
+
+        System.out.println(maximumBeauty(nums, k));
+    }
+}
