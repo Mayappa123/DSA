5693c550591903f03ff9e1ba393d02a98ea91ca3
Mayappa <mayappapujari561999@gmail.com>
2024-12-11 13:26:52 +0530

    added new code

--- arrays/Max_Beauty_of_an_Array.java
+++ arrays/Max_Beauty_of_an_Array.java
@@ -29,5 +29,12 @@ public class Max_Beauty_of_an_Array {
         int k = 2;
 
         System.out.println(maximumBeauty(nums, k)); // 3
+
+        // Explanation: In this example, we apply the following operations:
+        // Choose index 1, replace it with 4 (from range [4,8]), nums = [4,4,1,2].
+        // Choose index 3, replace it with 4 (from range [0,4]), nums = [4,4,1,4].
+        // After the applied operations, the beauty of the array nums is 3 (subsequence
+        // consisting of indices 0, 1, and 3).
+        // It can be proven that 3 is the maximum possible length we can achieve.
     }
 }
