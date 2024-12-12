ddfe639ce02b99dd00509cc3ae6a11afcd60bcfa
Mayappa <mayappapujari561999@gmail.com>
2024-12-12 13:19:55 +0530

    added new code

---
+++ PQ/Take_Gifts_From_Richest_Pile.java
@@ -0,0 +1,29 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Take_Gifts_From_Richest_Pile {
+
+    public static long pickGifts(int[] g, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int val : g)
+            maxHeap.add(val);
+
+        for (int i = 0; i < k && maxHeap.peek() > 1; i++) {
+            int x = maxHeap.remove();
+            maxHeap.add((int) Math.sqrt(x));
+        }
+
+        long sum = 0;
+        for (int val : maxHeap)
+            sum += val;
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] gifts = { 25, 64, 9, 4, 100 };
+        int k = 4;
+
+        System.out.println(pickGifts(gifts, k));
+    }
+}
