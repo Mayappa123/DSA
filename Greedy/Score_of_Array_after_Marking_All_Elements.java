2d4d739570c5c86d5fa68fcf87dc71267a9f2ea8
Mayappa <mayappapujari561999@gmail.com>
2024-12-13 12:42:53 +0530

    added new code

--- Greedy/Score_of_Array_after_Marking_All_Elements.java
+++ Greedy/Score_of_Array_after_Marking_All_Elements.java
@@ -37,6 +37,6 @@ public class Score_of_Array_after_Marking_All_Elements {
     public static void main (String[] args) {
        int[] nums = {2,1,3,4,5,2};
 
-       System.out.println(findScore(nums));
+       System.out.println(findScore(nums));  //7
     }
 }
