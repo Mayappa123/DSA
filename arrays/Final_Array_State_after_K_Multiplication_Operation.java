1dd8572db32e8c39d439e44df7a9ecca7f2255e7
Mayappa <mayappapujari561999@gmail.com>
2024-12-16 13:00:08 +0530

    added new code

--- arrays/Final_Array_State_after_K_Multiplication_Operation.java
+++ arrays/Final_Array_State_after_K_Multiplication_Operation.java
@@ -15,6 +15,8 @@ public class Final_Array_State_after_K_Multiplication_Operation {
     }
 
     public static void main(String[] args) {
-        
+   int[]nums = {2,1,3,5,6};
+   int k = 5;
+   int multiplier = 2;
     }
 }
