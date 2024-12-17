021e9a19d28c784e900ecb4879ee164ee8e5a1ed
Mayappa <mayappapujari561999@gmail.com>
2024-12-17 13:06:25 +0530

    added new code

--- Strings/Construct_String_with_Repeat_Limit.java
+++ Strings/Construct_String_with_Repeat_Limit.java
@@ -1,3 +1,12 @@
+//2182. Construct String With Repeat Limit
+
+// You are given a string s and an integer repeatLimit. 
+//Construct a new string repeatLimitedString using the characters of s such that no letter appears more than repeatLimit times in a row. 
+//You do not have to use all characters from s.
+// Return the lexicographically largest repeatLimitedString possible.
+// A string a is lexicographically larger than a string b if in the first position where a and b differ, string a has a letter that appears later in the alphabet than the corresponding letter in b. 
+//If the first min(a.length, b.length) characters do not differ, then the longer string is the lexicographically larger one.
+
 import java.util.Arrays;
 
 public class Construct_String_with_Repeat_Limit {
@@ -43,6 +52,20 @@ public class Construct_String_with_Repeat_Limit {
         int repeatLimit = 3;
 
         System.out.println(repeatLimitedString(s, repeatLimit)); // zzcccac
+
+        // Input: s = "cczazcc", repeatLimit = 3
+        // Output: "zzcccac"
+        // Explanation: We use all of the characters from s to construct the
+        // repeatLimitedString "zzcccac".
+        // The letter 'a' appears at most 1 time in a row.
+        // The letter 'c' appears at most 3 times in a row.
+        // The letter 'z' appears at most 2 times in a row.
+        // Hence, no letter appears more than repeatLimit times in a row and the string
+        // is a valid repeatLimitedString.
+        // The string is the lexicographically largest repeatLimitedString possible so
+        // we return "zzcccac".
+        // Note that the string "zzcccca" is lexicographically larger but the letter 'c'
+        // appears more than 3 times in a row, so it is not a valid repeatLimitedString.
 
     }
 }
