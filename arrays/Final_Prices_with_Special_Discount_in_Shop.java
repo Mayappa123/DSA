46237bbb10b81c6a42de6153c87ce646c726ef08
Mayappa <mayappapujari561999@gmail.com>
2024-12-18 11:55:42 +0530

    added new comment

---
+++ arrays/Final_Prices_with_Special_Discount_in_Shop.java
@@ -0,0 +1,24 @@
+public class Final_Prices_with_Special_Discount_in_Shop {
+
+    public static int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            res[i] = prices[i];
+
+            for (int j = i + 1; j < n; j++) {
+                if (prices[j] <= prices[i]) {
+                    res[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
