939e1baeaaafbe3f72726a01e7c9892ac4c3f2e8
Mayappa <mayappapujari561999@gmail.com>
2024-12-19 12:39:03 +0530

    added new comment

---
+++ arrays/Max_Chunks_to_Make_Sorted.java
@@ -0,0 +1,18 @@
+public class Max_Chunks_to_Make_Sorted {
+    public int maxChunksToSorted(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return 0;
+
+        int count = 0, max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+            if (max == i)
+                count++;
+        }
+
+        return count;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
