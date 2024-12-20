3ea08fa198c7cb6714c6fc4bd9e0716425d73a7b
Mayappa <mayappapujari561999@gmail.com>
2024-12-20 16:57:06 +0530

    Add changes in main branch

--- Recursion/Sum_of_first_n_natural_numbers.java
+++ Recursion/Sum_of_first_n_natural_numbers.java
@@ -1,15 +1,16 @@
 package Recursion;
 
 public class Sum_of_first_n_natural_numbers {
-    public static void Printsum(int i, int n, int sum){
-        if(i == n){
+    public static void Printsum(int i, int n, int sum) {
+        if (i == n) {
             sum += i;
             System.out.println(sum);
             return;
         }
         sum += i;
-        Printsum(i+1, n, sum);
+        Printsum(i + 1, n, sum);
     }
+
     public static void main(String[] args) {
         Printsum(1, 10, 0);
     }
