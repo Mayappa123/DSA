f0abb3602c312384d68e80f054bb706d3d12d601
Mayappa <mayappapujari561999@gmail.com>
2024-12-21 16:34:05 +0530

    4. added in feature branch

--- Binary Tree/Maximum_Number_of_K_Divisible_Components.java
+++ Binary Tree/Maximum_Number_of_K_Divisible_Components.java
@@ -55,5 +55,7 @@ public class Maximum_Number_of_K_Divisible_Components {
         int[][] edges = { { 0, 2 }, { 1, 2 }, { 1, 3 }, { 2, 4 } };
         int[] values = { 1, 8, 1, 4, 4 };
         int k = 6;
+
+        System.out.println(maxKDivisibleComponents(n, edges, values, k));
     }
 }
