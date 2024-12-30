7054b98a19aab4cd800b4ae4d7ef42152b899384
Mayappa <mayappapujari561999@gmail.com>
2024-12-30 18:13:28 +0530

    added new file

---
+++ Strings/Count_Ways_to_Build_good_Strings.java
@@ -0,0 +1,26 @@
+public class Count_Ways_to_Build_good_Strings {
+    private int mod = (int) 1e9 + 7;
+
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        if (zero > one)
+            return countGoodStrings(low, high, one, zero);
+
+        int dp[] = new int[high + 1], res = 0;
+        dp[0] = 1;
+
+        for (int i = 1; i <= high; i++) {
+            if (i >= zero)
+                dp[i] = (dp[i] + dp[i - zero]) % mod;
+            if (i >= one)
+                dp[i] = (dp[i] + dp[i - one]) % mod;
+            if (i >= low)
+                res = (res + dp[i]) % mod;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
