7ff34ad99ba904414ef4a0952d4b82e15ebd77c0
Mayappa <mayappapujari561999@gmail.com>
2025-01-01 11:22:11 +0530

    created file2

---
+++ Strings/Maximum_Score_After_Splitting_a_String.java
@@ -0,0 +1,10 @@
+// Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
+
+// The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
+
+public class Maximum_Score_After_Splitting_a_String {
+
+    public static void main(String[] args) {
+        
+    }
+}
