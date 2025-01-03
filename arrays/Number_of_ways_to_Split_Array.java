591531c2adef680d2fed46b5115513e1ad90cdf4
Mayappa <mayappapujari561999@gmail.com>
2025-01-03 17:54:17 +0530

    added in feature3

--- arrays/Number_of_ways_to_Split_Array.java
+++ arrays/Number_of_ways_to_Split_Array.java
@@ -1,5 +1,23 @@
 public class Number_of_ways_to_Split_Array {
+    public static int waysToSplitArray(int[] nums) {
+        long leftSum = 0, rightSum = 0;
+        for (int num : nums) {
+            rightSum += num;
+        }
+
+        int validSplits = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            leftSum += nums[i];
+            rightSum -= nums[i];
+            if (leftSum >= rightSum) {
+                validSplits++;
+            }
+        }
+
+        return validSplits;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
